1618db9fc79ffe62f014c46e2b99127e59ee69e2
adespaignet <adespaignet@gmail.com>
2025-04-01 00:52:46 +0400

    Remove environment files from Git tracking

--- src/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  triplitServerUrl: '__TRIPLIT_SERVER_URL__',
-  supabaseUrl: '__SUPABASE_URL__',
-  supabaseKey: '__SUPABASE_KEY__',
-};
--- src/environments/environment.production.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  triplitServerUrl: '__TRIPLIT_SERVER_URL__',
-  supabaseUrl: '__SUPABASE_URL__',
-  supabaseKey: '__SUPABASE_KEY__',
-};
-
-
